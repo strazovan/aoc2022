6d997f78f2004363fabed1c6900326a94271c7ce
Stražovan David <strazovan@sefira.cz>
2022-12-06 09:32:39 +0100

    6 simplified

--- 6/aoc6.py
+++ 6/aoc6.py
@@ -5,21 +5,14 @@
 # PART TWO
 WINDOW_LENGTH = 14
 
-def check_window(window):
-    return len(set(window)) == WINDOW_LENGTH
-
 with open("input.txt", "r") as file:
     input = file.readline()
-    # there is better way for sure than do the first window in a "hardcoded" way
+    # there is better way for sure than do the first window in a "hardcoded" way 
     floating_window = [*input[0:WINDOW_LENGTH]]
     print(floating_window)
-    if check_window(floating_window):
-        print(WINDOW_LENGTH)
-        exit()
-
-    for index in range(WINDOW_LENGTH, len(input)):
+    for index in range(0, len(input)):
         char = input[index]
         floating_window[index%WINDOW_LENGTH] = char
-        if check_window(floating_window) == True:
+        if len(set(floating_window)) == WINDOW_LENGTH:
             break
     print(index+1)
